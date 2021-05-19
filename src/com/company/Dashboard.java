ac241825f0b2abfa8600612173956fd609253f9f
Dendi Fazar Zaman <dendi.fazarzaman.tik19@mhsw.pnj.ac.id>
2021-05-19 13:48:28 +0700

    Remove Button
    Remove class button

--- src/com/company/Dashboard.java
+++ src/com/company/Dashboard.java
@@ -21,10 +21,10 @@ final class Dashboard extends JFrame implements Runnable{
 	
 	   
    private ShapeComponent shapeComponent = new ShapeComponent();
-   private JButton btn_shape;
-   private JButton btn_size;
-   private JButton btn_directional;
-   private JButton btn_speed;
+//   private JButton btn_shape;
+//   private JButton btn_size;
+//   private JButton btn_directional;
+//   private JButton btn_speed;
    private int tipe_shape = 1;
    private int size = 1;
    private int direction = 1;
@@ -58,93 +58,92 @@ final class Dashboard extends JFrame implements Runnable{
       this.shapeComponent.repaint();
    }
    
-
-   public final void btn_start() {
+   public final void start() {
       this.shapeComponent.start();
    }
 
-   public final void btn_stop() {
+   public final void stop() {
       this.shapeComponent.stop();
    }
-   
-   static JButton btn_shape(Dashboard dashboard) {
-	      return dashboard.btn_shape;
-	   }
-   
-   // sebagai incerement counter untuk tipe shape
-   public static int shapeCounter(Dashboard dashboard) {
-      return ++dashboard.tipe_shape;
-   }
-
-   // untuk membalikan counter ke nilai 1
-   static int shapeButtonDefault(Dashboard dashboard) {
-      return dashboard.tipe_shape;
-   }
-
-   // untuk membalikan variabel tipe_shape yang sudah diubah
-   static int shapeIndicator(Dashboard dashboard) {
-      return dashboard.tipe_shape;
-   }
-
-   // sebagai increment counter untuk size shape
-   static int sizeCounter(Dashboard dashboard) {
-      return ++dashboard.size;
-   }
-
-   // untuk membalikan variabel size ke nilai default yaitu 1
-   static int sizeIndicatorDefault(Dashboard dashboard) {
-      return dashboard.size;
-   }
-
-   // untuk membalikan variabel size yang sudah diubah
-   static int sizeIndicator(Dashboard dashboard) {
-      return dashboard.size;
-   }
-
-   // untuk merubah text button, yang dipanggil pada class SizeUtils
-   static JButton btn_size(Dashboard dashboard) {
-      return dashboard.btn_size;
-   }
-
-   // sebagai increment counter untuk direction
-   static int directionCounter(Dashboard dashboard) {
-      return ++dashboard.direction;
-   }
-
-   // untuk membalikan variabel direction ke nilai default yaitu 1
-   static int btn_start(Dashboard dashboard) {
-      return dashboard.direction;
-   }
-
-   // untuk membalikan variabel direction yang sudah diubah
-   static int directionIndicator(Dashboard dashboard) {
-      return dashboard.direction;
-   }
-
-   // untuk merubah text button, yang dipanggil pada class DirectionButtonUtils
-   static JButton btn_direction(Dashboard dashboard) {
-      return dashboard.btn_directional;
-   }
-
-   // sebagai increment counter untuk speed variabel
-   static int speedCounter(Dashboard dashboard) {
-      return ++dashboard.speed;
-   }
-
-   // untuk membalikan variabel speed ke nilai default yaitu 1
-   static int btn_stop(Dashboard dashboard) {
-      return dashboard.speed;
-   }
-
-   // untuk membalikan variabel speed yang sudah diubah
-   static int speedIndicator(Dashboard dashboard) {
-      return dashboard.speed;
-   }
-
-   // untuk merubah text button, yang dipanggil pada class SpeedButtonUtils
-   static JButton btn_speed(Dashboard dashboard) {
-      return dashboard.btn_speed;
-   }
+//   
+//   static JButton btn_shape(Dashboard dashboard) {
+//	      return dashboard.btn_shape;
+//	   }
+//   
+//   // sebagai incerement counter untuk tipe shape
+//   public static int shapeCounter(Dashboard dashboard) {
+//      return ++dashboard.tipe_shape;
+//   }
+//
+//   // untuk membalikan counter ke nilai 1
+//   static int shapeButtonDefault(Dashboard dashboard) {
+//      return dashboard.tipe_shape;
+//   }
+//
+//   // untuk membalikan variabel tipe_shape yang sudah diubah
+//   static int shapeIndicator(Dashboard dashboard) {
+//      return dashboard.tipe_shape;
+//   }
+//
+//   // sebagai increment counter untuk size shape
+//   static int sizeCounter(Dashboard dashboard) {
+//      return ++dashboard.size;
+//   }
+//
+//   // untuk membalikan variabel size ke nilai default yaitu 1
+//   static int sizeIndicatorDefault(Dashboard dashboard) {
+//      return dashboard.size;
+//   }
+//
+//   // untuk membalikan variabel size yang sudah diubah
+//   static int sizeIndicator(Dashboard dashboard) {
+//      return dashboard.size;
+//   }
+//
+//   // untuk merubah text button, yang dipanggil pada class SizeUtils
+//   static JButton btn_size(Dashboard dashboard) {
+//      return dashboard.btn_size;
+//   }
+//
+//   // sebagai increment counter untuk direction
+//   static int directionCounter(Dashboard dashboard) {
+//      return ++dashboard.direction;
+//   }
+//
+//   // untuk membalikan variabel direction ke nilai default yaitu 1
+//   static int btn_start(Dashboard dashboard) {
+//      return dashboard.direction;
+//   }
+//
+//   // untuk membalikan variabel direction yang sudah diubah
+//   static int directionIndicator(Dashboard dashboard) {
+//      return dashboard.direction;
+//   }
+//
+//   // untuk merubah text button, yang dipanggil pada class DirectionButtonUtils
+//   static JButton btn_direction(Dashboard dashboard) {
+//      return dashboard.btn_directional;
+//   }
+//
+//   // sebagai increment counter untuk speed variabel
+//   static int speedCounter(Dashboard dashboard) {
+//      return ++dashboard.speed;
+//   }
+//
+//   // untuk membalikan variabel speed ke nilai default yaitu 1
+//   static int btn_stop(Dashboard dashboard) {
+//      return dashboard.speed;
+//   }
+//
+//   // untuk membalikan variabel speed yang sudah diubah
+//   static int speedIndicator(Dashboard dashboard) {
+//      return dashboard.speed;
+//   }
+//
+//   // untuk merubah text button, yang dipanggil pada class SpeedButtonUtils
+//   static JButton btn_speed(Dashboard dashboard) {
+//      return dashboard.btn_speed;
+//   }
    
    void shapeSpeed(String line) {
 	   if (line.trim().matches(".*fast"))  {
@@ -210,27 +209,47 @@ final class Dashboard extends JFrame implements Runnable{
 				
 				Scanner ins = new Scanner(inStream);
 				PrintWriter out = new PrintWriter(outStream, true);
+
+				out.println("====================================================");
+				out.println("	Hello! Welcome To Bounce Play");
+				out.println("====================================================");
+				out.println("Command : start, stop, add, remove, exit");
+				out.println("Insert --help for detail of command's \n");
+
+				out.println("Insert Command");
 				
-				out.println("Hello! Enter BYE to exit.");
-				out.println("Command : start, stop, add, exit");
 				// echo client input
 				boolean done = false;
 				while (!done && ins.hasNextLine()) {
 					String line = ins.nextLine();
-					
-					out.println("Command " + ": " + line);
+
+					out.println("\nRunning " + ": " + line);
 					if (line.trim().equals("exit")) {
 						done = true;
-					} else if (line.trim().matches("add"))   {
+					} else if (line.trim().matches("add") )   {
 						out.println("Wrong Command : add [square|circle] [small|medium|large] [diagonal|horizontal|vertical] [fast|half|slow]");
 					} else if (line.trim().matches("add.* .* .* .*"))  {
 						shapeType(line);
 						addShape();
 					} else if (line.trim().equals("start") ) {
-						btn_start();
+						start();
 					}  else if (line.trim().equals("stop") ) {
-						btn_stop();
+						 stop();
+					} else if (line.trim().equals("remove") ) {
+						hapusShape();
+					} else if (line.trim().equals("--help") ) {
+						out.println("====================================================");
+						out.println("	    	Command List");
+						out.println("====================================================");
+						out.println("1. Add		: Add with option [square|circle] [small|medium|large] [diagonal|horizontal|vertical] [fast|half|slow]");
+						out.println("2. Remove	: Remove previous object");
+						out.println("3. Start	: Run Ball");
+						out.println("4. Stop		: Pause Ball");
+						out.println("5. Exit		: Exit Program");
+						out.println("====================================================");
 					}
+
+					out.println("\nInsert Command");
 				}
 			} finally {
 				System.exit(1);
